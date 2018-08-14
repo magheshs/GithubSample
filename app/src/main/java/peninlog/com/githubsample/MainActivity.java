a6c4b30c4906384c3c08eef69c93ae9f5c5bd58e
maghesh <mag.connect@yahoo.com>
2018-08-14 12:59:33 +0530

    Changes

--- app/src/main/java/peninlog/com/githubsample/MainActivity.java
+++ app/src/main/java/peninlog/com/githubsample/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "coming soon", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "another", Toast.LENGTH_SHORT).show();
     }
 }
